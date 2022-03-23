dca401ec96f1abb40fe53c322acc8d98d329583b
Piero <55633887+piero0920@users.noreply.github.com>
2022-03-23 02:22:50 -0400

    test

--- test-v6/script.js
+++ test-v6/script.js
@@ -1,7 +1,7 @@
 $(function() {
   var dataFile = [];
     
-  $.getJSON('https://test.jeanfernandez.repl.co/dataFile.json', function(data) {
+  $.getJSON('data.json', function(data) {
     $.each(data.dataFile, function(i, f) {
       var tblRow = "<tr>" + "<td>" + f.date + "</td>" + "<td>" + f.title + "</td>" + "<td>" + f.preview + "</td>"
       $(tblRow).appendTo("#userdata tbody");
